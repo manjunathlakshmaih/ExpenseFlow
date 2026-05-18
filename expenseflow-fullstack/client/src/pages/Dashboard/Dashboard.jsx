d106e0a6a30fa6306720a5f6ba715b26d60380a6
manjunath.l <manju.lakshmaiah.37@gmail.com>
2026-05-18 18:12:31 +0530

    new changes

---
+++ expenseflow-fullstack/client/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard =() => {
+    return (
+        <div>
+            <h1>Welcome to your Dashboard!</h1>
+            <p>This is where you can manage your expenses, view reports, and customize your settings.</p>
+        </div>
+    )
+}
+
+export default Dashboard;
